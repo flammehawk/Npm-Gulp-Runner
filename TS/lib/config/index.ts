6f234b1826724d80b8e5f32b6ccb12bb3233b292
flammehawk <57394581+flammehawk@users.noreply.github.com>
2020-03-16 22:04:33 +0100

    refactor: changed config to use extend with the interfaces

--- TS/lib/config/index.ts
+++ TS/lib/config/index.ts
@@ -19,257 +19,265 @@
 // cSpell:disable
 // tslint:disable
 
+export interface Config {
+  Targets: Targets;
+  Types: Types;
+  Folders: Folder[];
+}
 
-  export interface Config {
-    Targets: Targets;
-    Types: Types;
-    Folders: Folder[];
+export interface Folder extends Static {
+  Types: string[];
+  Src: string;
+}
+
+export interface Targets {
+  Dev: Build;
+  Build: Build;
+  Ci: Build;
+}
+
+export interface Build {
+  Slug: string;
+  Path: string;
+}
+
+export interface Types {
+  Styles: Scripts;
+  Scripts: Scripts;
+  Static: Static[];
+}
+
+export interface Scripts {
+  Sources: Source[];
+  Destination: string;
+}
+
+export interface Source {
+  Name: string;
+  Src: string | string[];
+  Exclude: string[];
+}
+
+export interface Static extends Source {
+  Dest: string;
+}
+
+// Converts JSON strings to/from your types
+// and asserts the results of JSON.parse at runtime
+export class Convert {
+  public static toConfig(json: string): Config {
+    return cast(JSON.parse(json), r('Config'));
   }
 
-  export interface Folder {
-    FolderName: string;
-    Src: string;
-    Dest: string;
-    Types: string[];
-    Exclude: string[];
+  public static configToJson(value: Config): string {
+    return JSON.stringify(uncast(value, r('Config')), null, 2);
+  }
+}
+
+function invalidValue(typ: any, val: any): never {
+  throw Error(
+    `Invalid value ${JSON.stringify(val)} for type ${JSON.stringify(typ)}`
+  );
+}
+
+function jsonToJSProps(typ: any): any {
+  if (typ.jsonToJS === undefined) {
+    var map: any = {};
+    typ.props.forEach((p: any) => (map[p.json] = { key: p.js, typ: p.typ }));
+    typ.jsonToJS = map;
+  }
+  return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ: any): any {
+  if (typ.jsToJSON === undefined) {
+    var map: any = {};
+    typ.props.forEach((p: any) => (map[p.js] = { key: p.json, typ: p.typ }));
+    typ.jsToJSON = map;
+  }
+  return typ.jsToJSON;
+}
+
+function transform(val: any, typ: any, getProps: any): any {
+  function transformPrimitive(typ: string, val: any): any {
+    if (typeof typ === typeof val) return val;
+    return invalidValue(typ, val);
   }
 
-  export interface Targets {
-    Dev: Build;
-    Build: Build;
-    Ci: Build;
-  }
-
-  export interface Build {
-    Slug: string;
-    Path: string;
-  }
-
-  export interface Types {
-    Styles: Scripts;
-    Scripts: Scripts;
-    Static: Static[];
-  }
-
-  export interface Scripts {
-    Sources: Source[];
-    Destination: string;
-  }
-
-  export interface Source {
-    Name: string;
-    Src: string;
-    Exclude: string[];
-  }
-
-  export interface Static {
-    Name: string;
-    Src: Src;
-    Dest: string;
-    Exclude: string[];
-  }
-
-  export type Src = string[] | string;
-
-  // Converts JSON strings to/from your types
-  // and asserts the results of JSON.parse at runtime
-  export class Convert {
-    public static toConfig(json: string): Config {
-      return cast(JSON.parse(json), r("Config"));
+  function transformUnion(typs: any[], val: any): any {
+    // val must validate against one typ in typs
+    var l = typs.length;
+    for (var i = 0; i < l; i++) {
+      var typ = typs[i];
+      try {
+        return transform(val, typ, getProps);
+      } catch (_) {}
     }
-
-    public static configToJson(value: Config): string {
-      return JSON.stringify(uncast(value, r("Config")), null, 2);
-    }
+    return invalidValue(typs, val);
   }
 
-  function invalidValue(typ: any, val: any): never {
-    throw Error(`Invalid value ${JSON.stringify(val)} for type ${JSON.stringify(typ)}`);
+  function transformEnum(cases: string[], val: any): any {
+    if (cases.indexOf(val) !== -1) return val;
+    return invalidValue(cases, val);
   }
 
-  function jsonToJSProps(typ: any): any {
-    if (typ.jsonToJS === undefined) {
-      var map: any = {};
-      typ.props.forEach((p: any) => (map[p.json] = { key: p.js, typ: p.typ }));
-      typ.jsonToJS = map;
-    }
-    return typ.jsonToJS;
+  function transformArray(typ: any, val: any): any {
+    // val must be an array with no invalid elements
+    if (!Array.isArray(val)) return invalidValue('array', val);
+    return val.map(el => transform(el, typ, getProps));
   }
 
-  function jsToJSONProps(typ: any): any {
-    if (typ.jsToJSON === undefined) {
-      var map: any = {};
-      typ.props.forEach((p: any) => (map[p.js] = { key: p.json, typ: p.typ }));
-      typ.jsToJSON = map;
+  function transformDate(typ: any, val: any): any {
+    if (val === null) {
+      return null;
     }
-    return typ.jsToJSON;
+    const d = new Date(val);
+    if (isNaN(d.valueOf())) {
+      return invalidValue('Date', val);
+    }
+    return d;
   }
 
-  function transform(val: any, typ: any, getProps: any): any {
-    function transformPrimitive(typ: string, val: any): any {
-      if (typeof typ === typeof val) return val;
-      return invalidValue(typ, val);
+  function transformObject(
+    props: { [k: string]: any },
+    additional: any,
+    val: any
+  ): any {
+    if (val === null || typeof val !== 'object' || Array.isArray(val)) {
+      return invalidValue('object', val);
     }
-
-    function transformUnion(typs: any[], val: any): any {
-      // val must validate against one typ in typs
-      var l = typs.length;
-      for (var i = 0; i < l; i++) {
-        var typ = typs[i];
-        try {
-          return transform(val, typ, getProps);
-        } catch (_) {}
+    var result: any = {};
+    Object.getOwnPropertyNames(props).forEach(key => {
+      const prop = props[key];
+      const v = Object.prototype.hasOwnProperty.call(val, key)
+        ? val[key]
+        : undefined;
+      result[prop.key] = transform(v, prop.typ, getProps);
+    });
+    Object.getOwnPropertyNames(val).forEach(key => {
+      if (!Object.prototype.hasOwnProperty.call(props, key)) {
+        result[key] = transform(val[key], additional, getProps);
       }
-      return invalidValue(typs, val);
-    }
-
-    function transformEnum(cases: string[], val: any): any {
-      if (cases.indexOf(val) !== -1) return val;
-      return invalidValue(cases, val);
-    }
-
-    function transformArray(typ: any, val: any): any {
-      // val must be an array with no invalid elements
-      if (!Array.isArray(val)) return invalidValue("array", val);
-      return val.map(el => transform(el, typ, getProps));
-    }
-
-    function transformDate(typ: any, val: any): any {
-      if (val === null) {
-        return null;
-      }
-      const d = new Date(val);
-      if (isNaN(d.valueOf())) {
-        return invalidValue("Date", val);
-      }
-      return d;
-    }
-
-    function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
-      if (val === null || typeof val !== "object" || Array.isArray(val)) {
-        return invalidValue("object", val);
-      }
-      var result: any = {};
-      Object.getOwnPropertyNames(props).forEach(key => {
-        const prop = props[key];
-        const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
-        result[prop.key] = transform(v, prop.typ, getProps);
-      });
-      Object.getOwnPropertyNames(val).forEach(key => {
-        if (!Object.prototype.hasOwnProperty.call(props, key)) {
-          result[key] = transform(val[key], additional, getProps);
-        }
-      });
-      return result;
-    }
-
-    if (typ === "any") return val;
-    if (typ === null) {
-      if (val === null) return val;
-      return invalidValue(typ, val);
-    }
-    if (typ === false) return invalidValue(typ, val);
-    while (typeof typ === "object" && typ.ref !== undefined) {
-      typ = typeMap[typ.ref];
-    }
-    if (Array.isArray(typ)) return transformEnum(typ, val);
-    if (typeof typ === "object") {
-      return typ.hasOwnProperty("unionMembers")
-        ? transformUnion(typ.unionMembers, val)
-        : typ.hasOwnProperty("arrayItems")
-        ? transformArray(typ.arrayItems, val)
-        : typ.hasOwnProperty("props")
-        ? transformObject(getProps(typ), typ.additional, val)
-        : invalidValue(typ, val);
-    }
-    // Numbers can be parsed by Date but shouldn't be.
-    if (typ === Date && typeof val !== "number") return transformDate(typ, val);
-    return transformPrimitive(typ, val);
+    });
+    return result;
   }
 
-  function cast<T>(val: any, typ: any): T {
-    return transform(val, typ, jsonToJSProps);
+  if (typ === 'any') return val;
+  if (typ === null) {
+    if (val === null) return val;
+    return invalidValue(typ, val);
   }
-
-  function uncast<T>(val: T, typ: any): any {
-    return transform(val, typ, jsToJSONProps);
+  if (typ === false) return invalidValue(typ, val);
+  while (typeof typ === 'object' && typ.ref !== undefined) {
+    typ = typeMap[typ.ref];
   }
-
-  function a(typ: any) {
-    return { arrayItems: typ };
+  if (Array.isArray(typ)) return transformEnum(typ, val);
+  if (typeof typ === 'object') {
+    return typ.hasOwnProperty('unionMembers')
+      ? transformUnion(typ.unionMembers, val)
+      : typ.hasOwnProperty('arrayItems')
+      ? transformArray(typ.arrayItems, val)
+      : typ.hasOwnProperty('props')
+      ? transformObject(getProps(typ), typ.additional, val)
+      : invalidValue(typ, val);
   }
+  // Numbers can be parsed by Date but shouldn't be.
+  if (typ === Date && typeof val !== 'number') return transformDate(typ, val);
+  return transformPrimitive(typ, val);
+}
 
-  function u(...typs: any[]) {
-    return { unionMembers: typs };
-  }
+function cast<T>(val: any, typ: any): T {
+  return transform(val, typ, jsonToJSProps);
+}
 
-  function o(props: any[], additional: any) {
-    return { props, additional };
-  }
+function uncast<T>(val: T, typ: any): any {
+  return transform(val, typ, jsToJSONProps);
+}
 
-  // function m(additional: any) {
-  //   return { props: [], additional };
-  // }
+function a(typ: any) {
+  return { arrayItems: typ };
+}
 
-  function r(name: string) {
-    return { ref: name };
-  }
+function u(...typs: any[]) {
+  return { unionMembers: typs };
+}
 
-  const typeMap: any = {
-    Config: o(
-      [
-        { json: "Targets", js: "Targets", typ: r("Targets") },
-        { json: "Types", js: "Types", typ: r("Types") },
-        { json: "Folders", js: "Folders", typ: a(r("Folder")) }
-      ],
-      false
-    ),
-    Folder: o(
-      [
-        { json: "FolderName", js: "FolderName", typ: "" },
-        { json: "Src", js: "Src", typ: "" },
-        { json: "Dest", js: "Dest", typ: "" },
-        { json: "Types", js: "Types", typ: a("") },
-        { json: "Exclude", js: "Exclude", typ: a("") }
-      ],
-      false
-    ),
-    Targets: o(
-      [
-        { json: "Dev", js: "Dev", typ: r("Build") },
-        { json: "Build", js: "Build", typ: r("Build") },
-        { json: "Ci", js: "Ci", typ: r("Build") }
-      ],
-      false
-    ),
-    Build: o([{ json: "Slug", js: "Slug", typ: "" }, { json: "Path", js: "Path", typ: "" }], false),
-    Types: o(
-      [
-        { json: "Styles", js: "Styles", typ: r("Scripts") },
-        { json: "Scripts", js: "Scripts", typ: r("Scripts") },
-        { json: "Static", js: "Static", typ: a(r("Static")) }
-      ],
-      false
-    ),
-    Scripts: o(
-      [{ json: "Sources", js: "Sources", typ: a(r("Source")) }, { json: "Destination", js: "Destination", typ: "" }],
-      false
-    ),
-    Source: o(
-      [
-        { json: "Name", js: "Name", typ: "" },
-        { json: "Src", js: "Src", typ: "" },
-        { json: "Exclude", js: "Exclude", typ: a("") }
-      ],
-      false
-    ),
-    Static: o(
-      [
-        { json: "Name", js: "Name", typ: "" },
-        { json: "Src", js: "Src", typ: u(a(""), "") },
-        { json: "Dest", js: "Dest", typ: "" },
-        { json: "Exclude", js: "Exclude", typ: a("") }
-      ],
-      false
-    )
-  };
+function o(props: any[], additional: any) {
+  return { props, additional };
+}
+
+// function m(additional: any) {
+//   return { props: [], additional };
+// }
+
+function r(name: string) {
+  return { ref: name };
+}
+
+const typeMap: any = {
+  Config: o(
+    [
+      { json: 'Targets', js: 'Targets', typ: r('Targets') },
+      { json: 'Types', js: 'Types', typ: r('Types') },
+      { json: 'Folders', js: 'Folders', typ: a(r('Folder')) }
+    ],
+    false
+  ),
+  Folder: o(
+    [
+      { json: 'FolderName', js: 'FolderName', typ: '' },
+      { json: 'Src', js: 'Src', typ: '' },
+      { json: 'Dest', js: 'Dest', typ: '' },
+      { json: 'Types', js: 'Types', typ: a('') },
+      { json: 'Exclude', js: 'Exclude', typ: a('') }
+    ],
+    false
+  ),
+  Targets: o(
+    [
+      { json: 'Dev', js: 'Dev', typ: r('Build') },
+      { json: 'Build', js: 'Build', typ: r('Build') },
+      { json: 'Ci', js: 'Ci', typ: r('Build') }
+    ],
+    false
+  ),
+  Build: o(
+    [
+      { json: 'Slug', js: 'Slug', typ: '' },
+      { json: 'Path', js: 'Path', typ: '' }
+    ],
+    false
+  ),
+  Types: o(
+    [
+      { json: 'Styles', js: 'Styles', typ: r('Scripts') },
+      { json: 'Scripts', js: 'Scripts', typ: r('Scripts') },
+      { json: 'Static', js: 'Static', typ: a(r('Static')) }
+    ],
+    false
+  ),
+  Scripts: o(
+    [
+      { json: 'Sources', js: 'Sources', typ: a(r('Source')) },
+      { json: 'Destination', js: 'Destination', typ: '' }
+    ],
+    false
+  ),
+  Source: o(
+    [
+      { json: 'Name', js: 'Name', typ: '' },
+      { json: 'Src', js: 'Src', typ: '' },
+      { json: 'Exclude', js: 'Exclude', typ: a('') }
+    ],
+    false
+  ),
+  Static: o(
+    [
+      { json: 'Name', js: 'Name', typ: '' },
+      { json: 'Src', js: 'Src', typ: u(a(''), '') },
+      { json: 'Dest', js: 'Dest', typ: '' },
+      { json: 'Exclude', js: 'Exclude', typ: a('') }
+    ],
+    false
+  )
+};
